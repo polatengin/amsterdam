5d66ca214c2ceebae4fae88e6f7bafc5f3b87049
polatengin <enpolat@microsoft.com>
2020-04-09 19:53:16 +0300

    if it's not already disabled, hudson private security realm disabled here

--- src/default-user.groovy
+++ src/default-user.groovy
@@ -4,3 +4,5 @@ import hudson.security.*
 def env = System.getenv()
 
 def jenkins = Jenkins.getInstance()
+if(!(jenkins.getSecurityRealm() instanceof HudsonPrivateSecurityRealm))
+    jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
