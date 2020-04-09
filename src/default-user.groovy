1812fe4a3559c7e3f5e17c27e531d0e1826a7c09
polatengin <enpolat@microsoft.com>
2020-04-09 19:54:15 +0300

    if authorization strategy is not configured already, it's global matrix authorization strategy now

--- src/default-user.groovy
+++ src/default-user.groovy
@@ -6,3 +6,6 @@ def env = System.getenv()
 def jenkins = Jenkins.getInstance()
 if(!(jenkins.getSecurityRealm() instanceof HudsonPrivateSecurityRealm))
     jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+
+if(!(jenkins.getAuthorizationStrategy() instanceof GlobalMatrixAuthorizationStrategy))
+    jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
